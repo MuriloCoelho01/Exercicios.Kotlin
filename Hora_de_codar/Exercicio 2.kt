b3f72e3db08282cfa61a981a9b8d401218bb10a2
Murilo Coelho <coelho.murilo10@gmail.com>
2024-08-25 00:41:51 -0300

    Exercicios Hora de codar feito em Kotlin

---
+++ Hora_de_codar/Exercicio 2.kt
@@ -0,0 +1,8 @@
+fun main(){
+    var nome_usuario: String
+
+    println("Qual seu nome?")
+    nome_usuario = readln()
+
+    println("Olá $nome_usuario!")
+}
---
+++ Hora_de_codar/Área do Paralelogramo.kt
@@ -0,0 +1,12 @@
+fun main(){
+    var altura: Int
+    var base: Int
+
+    println("Qual a altura?")
+    altura = readln().toInt()
+
+    println("Qual a base?")
+    base = readln().toInt()
+
+    println("A area do retângulo é " + altura * base )
+}
