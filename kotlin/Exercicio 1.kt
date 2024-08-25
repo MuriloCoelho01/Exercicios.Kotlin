83e22b5cdd87b9eb16f47a9eef4f5c4c41e4878e
Murilo Coelho <coelho.murilo10@gmail.com>
2024-08-25 00:40:21 -0300

    Delete kotlin directory

--- kotlin/Exercicio 1.kt
+++
@@ -1,9 +0,0 @@
-fun main (){
-    var nome_do_carro: String
-
-    println("Qual seu carro?")
-    nome_do_carro = readln()
-
-    println("Seu carro é $nome_do_carro")
-}
-
--- kotlin/Exercicio 2.kt
+++
@@ -1,8 +0,0 @@
-fun main(){
-    var nome_usuario: String
-
-    println("Qual seu nome?")
-    nome_usuario = readln()
-
-    println("Olá $nome_usuario!")
-}
